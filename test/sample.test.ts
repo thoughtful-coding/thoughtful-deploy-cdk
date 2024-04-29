8676778359b5b1169d04d94e33d671c28c42aa31
Eric Rizzi <eric.rizzi@gmail.com>
2024-04-29 14:11:33 -0500

    Fix unit tests

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as Sample from '../lib/sample-stack';
+import * as Sample from '../lib/stacks/resourceStack';
 
 test('SQS Queue and Lambda created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new Sample.SampleStack(app, 'MyTestStack');
+  const stack = new Sample.ResourceStack(app, 'MyTestStack');
   // THEN
 
   const template = Template.fromStack(stack);

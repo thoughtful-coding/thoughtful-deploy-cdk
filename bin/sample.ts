d8dbfb46dda0ec9fd37271ea5cd069fe2f314034
Eric Rizzi <eric.rizzi@gmail.com>
2025-05-23 16:34:21 -0500

    Improve deploy and turn on compute

--- bin/sample.ts
+++ bin/sample.ts
@@ -9,16 +9,16 @@ import { OverviewStack } from '../lib/stacks/overview-stack';
 const app = new cdk.App();
 const envProps = CdkConfig.getEnvironment();
 
-// const imageTag = app.node.tryGetContext('imageTag') as string | undefined;
-// if (!imageTag && process.env.CI) {
-//   throw new Error(
-//     "Context variable 'imageTag' must be passed to the CDK process in CI."
-//   );
-// } else if (!imageTag) {
-//   console.warn(
-//     "Warning: Context variable 'imageTag' was not provided. Using 'latest' as fallback for Lambda image tag."
-//   );
-// }
+const imageTag = app.node.tryGetContext('imageTag') as string | undefined;
+if (!imageTag && process.env.CI) {
+  throw new Error(
+    "Context variable 'imageTag' must be passed to the CDK process in CI."
+  );
+} else if (!imageTag) {
+  console.warn(
+    "Warning: Context variable 'imageTag' was not provided. Using 'latest' as fallback for Lambda image tag."
+  );
+}
 
 const foundationalStack = new FoundationalResourcesStack(
   app,
@@ -32,16 +32,16 @@ const storageStack = new StorageStack(app, 'SampleStorageStack', {
   envProps: envProps,
 });
 
-// const lambdaComputeStack = new ComputeStack(app, 'SampleLambdaComputeStack', {
-//   envProps: envProps,
-//   dockerRepository: foundationalStack.dockerRepository,
-//   imageTag: imageTag || 'latest', // Ensure this fallback is acceptable or handle error
-//   inputBucket: storageStack.inputBucket,
-//   outputBucket: storageStack.outputBucket,
-//   transformationCounterTable: storageStack.transformationCounterTable,
-//   userProgressTable: storageStack.userProgressTable,
-//   learningEntriesTable: storageStack.learningEntriesTable,
-// });
+const lambdaComputeStack = new ComputeStack(app, 'SampleLambdaComputeStack', {
+  envProps: envProps,
+  dockerRepository: foundationalStack.dockerRepository,
+  imageTag: imageTag || 'latest', // Ensure this fallback is acceptable or handle error
+  inputBucket: storageStack.inputBucket,
+  outputBucket: storageStack.outputBucket,
+  transformationCounterTable: storageStack.transformationCounterTable,
+  userProgressTable: storageStack.userProgressTable,
+  learningEntriesTable: storageStack.learningEntriesTable,
+});
 
 // const apiGatewayStack = new APIGatewayStack(app, 'SampleApiGatewayStack', {
 //   envProps: envProps,

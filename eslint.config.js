e1256abac06970a3a9e0774d59aa6811541b8e9c
Eric Rizzi <eric.rizzi@gmail.com>
2025-05-23 13:44:20 -0500

    Linting stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 // @ts-check
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierRecommended,
 );

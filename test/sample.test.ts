5d6c76810cf6a7a5d1531ee0e97a8c1b15836df5
Eric Rizzi <eric.rizzi@gmail.com>
2025-05-23 14:26:31 -0500

    Null test

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,12 +1,3 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import {ResourceStack} from '../lib/stacks/resourceStack';
-
 test('SQS Queue and Lambda created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new ResourceStack(app, 'MyTestStack', {envProps: {account: "1111", "region": "us-east-2"}});
-  // THEN
-
-  const template = Template.fromStack(stack);
+  console.log('hi!');
 });

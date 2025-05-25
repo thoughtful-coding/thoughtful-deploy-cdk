a79f1c8048d16a2d4ebe29b3e10a1a68b031b5ec
Eric Rizzi <eric.rizzi@gmail.com>
2025-05-25 17:38:24 -0500

    With broken stack... try and push API lower

--- bin/sample.ts
+++ bin/sample.ts
@@ -26,22 +26,22 @@ const storageStack = new StorageStack(app, 'SampleStorageStack', {
   envProps: envProps,
 });
 
-const lambdaComputeStack = new ComputeStack(app, 'SampleLambdaComputeStack', {
-  envProps: envProps,
-  dockerRepository: foundationalStack.dockerRepository,
-  imageTag: imageTag || 'latest', // Ensure this fallback is acceptable or handle error
-  outputBucket: storageStack.outputBucket,
-  transformationCounterTable: storageStack.transformationCounterTable,
-  userProgressTable: storageStack.userProgressTable,
-  learningEntriesTable: storageStack.learningEntriesTable,
-  httpApi: storageStack.httpApi,
-});
+// const lambdaComputeStack = new ComputeStack(app, 'SampleLambdaComputeStack', {
+//   envProps: envProps,
+//   dockerRepository: foundationalStack.dockerRepository,
+//   imageTag: imageTag || 'latest', // Ensure this fallback is acceptable or handle error
+//   outputBucket: storageStack.outputBucket,
+//   transformationCounterTable: storageStack.transformationCounterTable,
+//   userProgressTable: storageStack.userProgressTable,
+//   learningEntriesTable: storageStack.learningEntriesTable,
+//   httpApi: storageStack.httpApi,
+// });
 
-const apiGatewayStack = new APIGatewayStack(app, 'SampleApiGatewayStack', {
-  apiTransformationLambda: lambdaComputeStack.apiTransformationLambda,
-  userProgressLambda: lambdaComputeStack.userProgressLambda,
-  learningEntriesLambda: lambdaComputeStack.learningEntriesLambda,
-});
+// const apiGatewayStack = new APIGatewayStack(app, 'SampleApiGatewayStack', {
+//   apiTransformationLambda: lambdaComputeStack.apiTransformationLambda,
+//   userProgressLambda: lambdaComputeStack.userProgressLambda,
+//   learningEntriesLambda: lambdaComputeStack.learningEntriesLambda,
+// });
 
 // const overviewStack = new OverviewStack(app, 'SampleOverviewStack', {
 //   apiTransformationLambda: lambdaComputeStack.apiTransformationLambda,

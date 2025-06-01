cb6d210c07dd0b9a32637346a29c8ea3c3fc7238
Eric Rizzi <eric.rizzi@gmail.com>
2025-06-01 14:20:43 -0500

    Add another route to InstructorLambda

--- lib/stacks/api-gateway-stack.ts
+++ lib/stacks/api-gateway-stack.ts
@@ -91,6 +91,14 @@ export class APIGatewayStack extends Stack {
       authorizer: googleJwtAuthorizer,
     });
 
+    new ApiRoute(this, 'InstructorStudentUnitProgressRoute', {
+      httpApi: this.httpApi,
+      routePath: '/instructor/students/{studentId}/units/{unitId}/progress',
+      methods: [apigwv2.HttpMethod.GET],
+      handler: props.instructorPortalLambda,
+      authorizer: googleJwtAuthorizer,
+    });
+
     // CloudFormation Output for the API endpoint
     new CfnOutput(this, 'ApiEndpointOutput', {
       value: this.apiEndpoint,

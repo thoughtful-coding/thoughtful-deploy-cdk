c67f191dc4c07c3476dcbedc79e106f5ca2f9d35
Eric Rizzi <erizzi@amazon.com>
2025-12-05 08:04:07 -0500

    Remove secrets manager

--- lib/stacks/foundational-resources-stack.ts
+++ lib/stacks/foundational-resources-stack.ts
@@ -1,7 +1,6 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
-import { ManagedSecret } from '../constructs/secret-manager';
 import { EnvironmentProps } from '../utils/config';
 
 export interface FoundationalResourcesStackProps extends StackProps {
@@ -10,8 +9,6 @@ export interface FoundationalResourcesStackProps extends StackProps {
 
 export class FoundationalResourcesStack extends Stack {
   public readonly dockerRepository: ecr.IRepository;
-  public readonly chatbotApiKeySecret: ManagedSecret;
-  public readonly jwtSecret: ManagedSecret;
 
   constructor(scope: Construct, id: string, props: FoundationalResourcesStackProps) {
     super(scope, id, props);
@@ -29,23 +26,6 @@ export class FoundationalResourcesStack extends Stack {
       value: this.dockerRepository.repositoryUri,
       description: 'The URI of the ECR Docker repository',
       exportName: 'DockerRepositoryUri', // Optional: makes it easier to import in other stacks if not passing the stack object directly
-    });
-
-    // Secrets
-
-    this.chatbotApiKeySecret = new ManagedSecret(this, 'AppChatBotApiKey', {
-      secretName: '/thoughtful-python/chatbot-api-key',
-    });
-
-    this.jwtSecret = new ManagedSecret(this, 'AppJwtSecretKey', {
-      secretName: '/thoughtful-python/jwt-secret-key',
-      // It's recommended to have CDK generate a strong secret for you
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({ username: 'jwt-user' }),
-        generateStringKey: 'password',
-        passwordLength: 32,
-        excludePunctuation: true,
-      },
     });
   }
 }
